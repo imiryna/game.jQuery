546e5df5e29a7a64b39a7444cb7847db8f24a453
Iryna Burlaka <i.am.blinova@gmail.com>
2024-06-25 18:05:59 +0200

    next step

--- memory.js
+++ memory.js
@@ -1,25 +1,39 @@
-const cards = ["ciri.png", "geralt.png", "ciri.png", "iorweth.png", "jaskier.png", "jasker.png", "triss.png", "yen.png", "gerald.png", "iorweth.png", "triss.png", "yen.png"];
+const cards = ["ciri.png", "geralt.png", "ciri.png", "iorweth.png", "jaskier.png", "jaskier.png", "triss.png", "yen.png", "geralt.png", "iorweth.png", "triss.png", "yen.png"];
 
 const cardsWtaper = document.querySelector(".board");
 
+function memori() {
+  cards.forEach((img) => {
+    const cardElement = document.createElement("img");
+    cardElement.src = `img/${img}`;
+    // cardElement.document.querySelector(".card").innerHTML = cardElement;
+  });
+  return;
+}
+
 cardsWtaper.addEventListener("click", handleClick);
 
-function createImg() {
-  cards.forEach((element, index) => {
-    const cardElement = document.createElement("img");
-    cardElement.src = `img/${element}`;
-    alert(element);
-  });
-}
-
 function handleClick(e) {
-  if (e.target.nodeName === "IMG") {
-    const png = e.target[0];
-    console.log(png);
-  }
+  const el = e.target.id;
+  const cardElement = document.createElement("img");
+  cardElement.src = `img/${cards[Number(el)]}`;
+  document.getElementById(el).appendChild(cardElement);
 }
 
-function ckick(nr) {
-  alert(nr);
-}
-window.onload = createImg;
+// function createImg() {
+//   cards.forEach((element, index) => {
+//     const cardElement = document.createElement("img");
+//     cardElement.src = `img/${element}`;
+//   });
+//   cardElement.addEventListener("click", handleClick);
+//   cardsWtaper.appendChild(cardElement);
+// }
+
+// function handleClick(e) {
+//   if (e.target.nodeName === "IMG") {
+//     const png = e.target.id;
+//     console.log(png);
+//   }
+// }
+
+// window.onload = handleClick;
